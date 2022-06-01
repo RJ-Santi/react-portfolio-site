d9c8afa00c4254fc90d0efb5b17b32c68ed4144d
RJ-Santi <cr7rs7@gmail.com>
2022-06-01 05:03:31 -0400

    Add projects component. Organize css and add images.

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -6,6 +6,7 @@ function Footer() {
       <Container fluid className="footer-container pt-5">
         <a className="footer-link hoverColor" href="https://www.linkedin.com/in/ryan-santiago/" target="_blank">LinkedIn</a>
         <a className="footer-link hoverColor" href="http://www.ryansantiago.me" target="_blank">Website</a>
+        <a className="footer-link hoverColor" href="https://github.com/RJ-Santi?tab=repositories" target="_blank">Github</a>
         <p className="copyright">© 2021 Ryan Santiago.</p>
       </Container>
     </>
