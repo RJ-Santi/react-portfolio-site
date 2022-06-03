b87e691dd8bbf935a7d5d193308ba58951581390
Ryan Santiago <rjsanti13@gmail.com>
2022-06-03 10:51:21 -0400

    Complete contact form with emailJS

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,20 @@
+import { useRef } from 'react'
+import emailjs from 'emailjs-com'
+
 function Contact() {
+  const form = useRef()
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs.sendForm('service_1il4qgm', 'template_0pp66on', form.current, 'P2F4byEv7ghzE8BUK')
+    .then((result) => {
+        console.log(result.text)
+    }, (error) => {
+        console.log(error.text)
+    })
+  }
+
   return (
     <div>
       <div id="contact" className="contact-me p-5 row">
@@ -11,25 +27,25 @@ function Contact() {
 
 
         <div className="col-md-6 py-5">
-            <form id="contact-form" method="POST" action="send">
-              <label>Name</label>
-              <input className="form-control" id="name" type="text" name="name" required />
+          <form ref={form} onSubmit={sendEmail} id="contact-form">
+            <label>Name</label>
+            <input className="form-control" id="name" type="text" name="user_name" required />
 
-              <label>Subject</label>
-              <input className="form-control" id="subject" type="text" name="subject" required />
+            <label>Subject</label>
+            <input className="form-control" id="subject" type='text' name='subject' required />
 
-              <label>Email</label>
-              <input className="form-control" id="email" type="text" name="email" required />
+            <label>Email</label>
+            <input className="form-control" id="email" type="email" name="user_email" required />
 
-              <label>Message</label>
-              <textarea className="form-control" id="message" name="message" required></textarea>
+            <label>Message</label>
+            <textarea className="form-control" id="message" name="message" required />
 
-              <div className="d-grid mx-auto mt-2">
-                <button id="submit-btn" className="btn" type="submit" value="submit">Send</button>
-              </div>
-            </form>
-          </div>
+            <div className="d-grid mx-auto mt-2">
+              <input id="submit-btn" className="btn" type="submit" value="Send" />
+            </div>
+          </form>
         </div>
+      </div>
     </div>
   )
 }
